8277c7b88da9a1ef30b98d875f1e661f43117fd9
Agusfiorda <agus.fiorda.99@gmail.com>
2020-04-02 22:36:45 -0300

    agrego opciones a la calculadora

--- ejercicios/Condicionales_Ejercicios.py
+++ ejercicios/Condicionales_Ejercicios.py
@@ -6,12 +6,17 @@ restar,multiplicar y dividir).El usuario debe especificar la operacion
 con el primer caracter del nombre de la operacion
 """
 
-op= input(f"Elija la operacion que va a utilizar(sumar/restar/div/multi): ")
+op= input(f"Elija la operacion que va a utilizar(s 'suma'/r 'resta'/d 'division'/m 'multiplicacion'): ").upper()
 
-if op=="sumar":
+if op=="s":
     n1=int(input("Escriba el primer numero: "))
     n2=int(input("Escriba el segundo numero: "))
     res=n1+n2
     print(f"El resultado es: [{res}]")
 
-
+elif op=="r":
+    n1=int(input("Escriba el primer numero: "))
+    n2=int(input("Escriba el segundo numero: "))
+    res=n1-n2
+    print(f"El resultado es: [{res}]")
+elif op=="m"
