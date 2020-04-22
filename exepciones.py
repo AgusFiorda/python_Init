bab910c3e8677d4355d39caf67d1224a99aded81
AgusFiorda <agus.fiorda.99@gmail.com>
2020-04-21 22:47:11 -0300

    mas pruebas de excepciones

--- exepciones.py
+++ exepciones.py
@@ -20,10 +20,20 @@ def divide(num1, num2):
     except ZeroDivisionError:  #se ejecuta solo si esta mal lo que esta lo del try"
         print("No se puede dividir entre 0")
         return "Operacion erronea"
+    #except:
+    #Si se pone solamente el except solo sin ningun error , es decir que hay un error general detecta un error pero no sabe cual  es el tipo de error.
+    #finally:
+    #se pone finally para que lo ejecute si o si despues del except, se va a ejecutar siempre
+    #tambien se puede poner otro except y otro tipo de error por si se equivoca de otra forma, poniendo letras por ejemlpo
 """esto sirve para que el resto del programa no se caiga por un error que esta antes"""
-op1 = (int(input("Introduce el primer n�mero: ")))
+while True:
+    try:
+        op1 = (int(input("Introduce el primer n�mero: ")))
 
-op2 = (int(input("Introduce el segundo n�mero: ")))
+        op2 = (int(input("Introduce el segundo n�mero: ")))
+        break #usamos el while para que valide los dos valores y si son validados sigue con el programa
+    except ValueError:
+        print("Los valores introducidos no son correctos. intentelo denuevo")
 
 operacion = input("Introduce la operaci�n a realizar (suma,resta,multiplica,divide): ")
 
