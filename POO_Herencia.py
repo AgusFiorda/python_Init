bc9ad2ecb536394c1dcfaa620ca5a982782d8340
AgusFiorda <agus.fiorda.99@gmail.com>
2020-04-24 00:44:04 -0300

    POO HERENCIA

--- POO_Herencia.py
+++ POO_Herencia.py
@@ -23,9 +23,54 @@ class Vehiculos():
               f"En marcha: {self.enmarcha}\nAcelerando: {self.acelera}"
               f"\nFrenando: {self.frena}")
 
+class Furgoneta(Vehiculos):
+
+    def carga(self,cargar):
+        self.cargado=cargar
+        if(self.cargado):
+            return "La furgoneta esta cargada"
+        else:
+            return "La furgoneta no esta cargada"
+
+
 class Moto(Vehiculos): #para heredar se escribe dentro de los parentesis la clase que va a heredar la nueva clase
+    hwilly=""
+    def willy(self):
+        self.hwilly="Voy haciendo willy"
+
+    def estado(self): #sobre escritura de metodos
+        print(f"Marca: {self.marca} \n Modelo: {self.modelo}\n"
+              f"En marcha: {self.enmarcha}\nAcelerando: {self.acelera}"
+              f"\nFrenando: {self.frena}\n{self.hwilly}")
+
+
+class VElectricos(Vehiculos):
+    def __init__(self,marca,modelo):
+        super().__init__(marca,modelo) #Super() llama al metodo de la clase padre
+
+        self.autonomia=100
+
+    def cargaEnergia(self):
+        self.cargando==True
+
+
+print("---------------MOTO-----------------")
+miMoto=Moto("Honda","Tornado")
+miMoto.willy()
+miMoto.estado()
+
+print("---------------FURGONETA-----------------")
+miFurgoneta=Furgoneta("Renault","Kangoo")
+miFurgoneta.arrancar()
+miFurgoneta.estado()
+print(miFurgoneta.carga(True))
+
+print("---------------VehiculosELECTRICOS-----------------")
+class BicicletaElectrica(VElectricos,Vehiculos): #Herencia multiple
     pass
 
-miMoto=Moto("Honda","Tornado")
-miMoto.estado()
+
+
+miBici=BicicletaElectrica("Orbea","hjd5")
+
 
